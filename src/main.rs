69e556cf5a77c75585a8cc289be374ffa038456f
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-05-05 15:52:30 -0500

    refactored and renamed the priority order route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub mod gtm;
+pub mod healthcheck;
 
 use axum::{
     extract::{Query, State},
@@ -32,13 +32,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
-    let cache: Arc<Mutex<HashMap<String, Vec<gtm::Member>>>> = Arc::new(Mutex::new(HashMap::new()));
+    let cache: Arc<Mutex<HashMap<String, Vec<healthcheck::Member>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+
+    // TODO(alb): Separate into multiple IP info routes by type
+    // e.g. "global availability", "round robin", "random"
 
     let t = Arc::clone(&cache);
     let app = Router::new()
         .route("/healthz", get(healthz))
         .route("/livez", get(livez))
         .route("/info", get(info))
+        .route("/priority-order", get(info))
+        .route("/randommember", get(info))
         .route("/reset", get(reset))
         .with_state(t);
 
@@ -79,9 +85,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for c in &conf {
         let t = Arc::clone(&cache);
         let mut items = t.lock().unwrap();
-        let mut members: Vec<gtm::Member> = c.members.iter().map(|m| gtm::Member::new(m)).collect();
+        let mut members: Vec<healthcheck::Member> = c
+            .members
+            .iter()
+            .map(|m| healthcheck::Member::new(m))
+            .collect();
         if let Some(fallback_ip) = c.fallback_ip {
-            members.push(gtm::Member {
+            members.push(healthcheck::Member {
                 host: "fallback".into(),
                 ip: fallback_ip,
                 healthy: true,
@@ -147,11 +157,11 @@ async fn livez() -> (StatusCode, &'static str) {
 /// Get the current value of the "localhost" entry of the host:ip Map
 async fn info(
     q: Query<QueryParams>,
-    State(state): State<Arc<Mutex<HashMap<String, Vec<gtm::Member>>>>>,
+    State(state): State<Arc<Mutex<HashMap<String, Vec<healthcheck::Member>>>>>,
 ) -> (StatusCode, String) {
     let map = &state.lock().unwrap();
     if let Some(item) = map.get(&q.name) {
-        let healthy_members: Vec<&gtm::Member> =
+        let healthy_members: Vec<&healthcheck::Member> =
             item.iter().filter(|m| m.healthy == true).collect();
 
         if let Some(member) = healthy_members.first() {
@@ -167,15 +177,72 @@ async fn info(
     }
 }
 
+/// Handler for the priority-order route. Returns the first healthy pool member or the fallback if
+/// necessary
+async fn handle_priority_order(
+    q: Query<QueryParams>,
+    State(state): State<Arc<Mutex<HashMap<String, Vec<healthcheck::Member>>>>>,
+) -> (StatusCode, String) {
+    let state = &state.lock();
+    let map = match state {
+        Ok(m) => m,
+        Err(_) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal Server Error".into(),
+            )
+        }
+    };
+
+    let members = match map.get(&q.name) {
+        Some(p) => {
+            let healthy_members: Vec<&healthcheck::Member> =
+                p.iter().filter(|m| m.healthy == true).collect();
+            healthy_members
+        }
+        None => return (StatusCode::NOT_FOUND, "Pool not found".into()),
+    };
+
+    if let Some(member) = members.first() {
+        (StatusCode::OK, member.ip.to_string())
+    } else {
+        (
+            StatusCode::NOT_FOUND,
+            "No healthy members and no fallback IP".into(),
+        )
+    }
+}
+
+
+//TODO(alb): finish the random order version (or round robin)
+
+/// Handler for the random-member route. Returns a random selection from the healthy members
+// async fn handle_random_order(
+//     q: Query<QueryParams>,
+//     State(state): State<Arc<Mutex<HashMap<String, Vec<healthcheck::Member>>>>>,
+// ) -> (StatusCode, String) {
+//     let state = &state.lock();
+//
+//     let map = match state {
+//         Ok(m) => m,
+//         Err(_) => {
+//             return (
+//                 StatusCode::INTERNAL_SERVER_ERROR,
+//                 "
+//
+//         }
+//     }
+// }
+
 /// Set the contents of the "localhost" entry of the host:ip map to be some
 /// arbitrary IP to prove that the state is changing
 async fn reset(
     q: Query<QueryParams>,
-    State(state): State<Arc<Mutex<HashMap<String, Vec<gtm::Member>>>>>,
+    State(state): State<Arc<Mutex<HashMap<String, Vec<healthcheck::Member>>>>>,
 ) -> (StatusCode, String) {
     state.lock().unwrap().insert(
         q.name.clone(),
-        vec![gtm::Member {
+        vec![healthcheck::Member {
             host: String::from("localhost"),
             ip: Into::into([1, 2, 3, 4]),
             healthy: true,
@@ -186,12 +253,12 @@ async fn reset(
 }
 
 /// Read config file
-fn read_config(path: String) -> Result<Vec<gtm::Pool>, Box<dyn Error>> {
+fn read_config(path: String) -> Result<Vec<healthcheck::Pool>, Box<dyn Error>> {
     // let mut conf: Vec<gtm::Pool> = Vec::new();
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    let conf: Vec<gtm::Pool> = serde_json::from_reader(reader)?;
+    let conf: Vec<healthcheck::Pool> = serde_json::from_reader(reader)?;
 
     Ok(conf)
 }
